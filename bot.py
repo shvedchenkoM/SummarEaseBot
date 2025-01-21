4d8da41ba0c9eb89fb1ca614111cf3f5ca1b7cb5
shvedchenkoM <mashylka81@gmail.com>
2025-01-21 23:04:01 +0100

    CL1: YOLO summarizer

--- bot.py
+++ bot.py
@@ -1,9 +1,9 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
-# from summarizer import summarize_messages  # Import the summarize function
+from summarizer import summarize_messages  # Import the summarize function
 import os
 from dotenv import load_dotenv
-
+load_dotenv()
 TELEGRAM_API_TOKEN = os.getenv("BOT_TOKEN")
 messages = []
 print(TELEGRAM_API_TOKEN)
@@ -22,15 +22,15 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     )
 
 # /summarize command
-# async def summarize(update: Update, context: ContextTypes.DEFAULT_TYPE):
-#     global messages
-#     if not messages:
-#         await update.message.reply_text("❌ No messages to summarize yet! Send some messages first.")
-#         return
-#
-#     summary = summarize_messages(messages)
-#     await update.message.reply_text(f"📄 Summary:\n{summary}")
-#     messages = [] TODO: fix
+async def summarize(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    global messages
+    if not messages:
+        await update.message.reply_text("❌ No messages to summarize yet! Send some messages first.")
+        return
+
+    summary = summarize_messages(messages)
+    await update.message.reply_text(f"📄 Summary:\n{summary}")
+    messages = []
 
 # /show_messages command
 async def show_messages(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -56,7 +56,7 @@ def main():
     application.add_handler(CommandHandler("start", start))
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, collect_messages))
     application.add_handler(CommandHandler("show_messages", show_messages))
-    # application.add_handler(CommandHandler("summarize", summarize))
+    application.add_handler(CommandHandler("summarize", summarize))
 
     print("🤖 Bot is running...")
     application.run_polling()
